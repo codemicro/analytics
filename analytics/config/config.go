23d59f96bfcc277db775e18389d723b968034dc3
AKP <tom@tdpain.net>
2023-04-05 14:13:49 +0100

    Remove unused config params
    Signed-off-by: AKP <tom@tdpain.net>

--- analytics/config/config.go
+++ analytics/config/config.go
@@ -13,12 +13,6 @@ type Config struct {
 	Database struct {
 		DSN string
 	}
-	HTTP struct {
-		Address string
-	}
-	Datasette struct {
-		Address string
-	}
 }
 
 func Load() (*Config, error) {
@@ -29,9 +23,7 @@ func Load() (*Config, error) {
 
 	conf := new(Config)
 	conf.Ingest.Address = asString(cl.withDefault("ingest.address", "127.0.0.1:7500"))
-	conf.HTTP.Address = asString(cl.withDefault("http.address", "127.0.0.1:8080"))
 	conf.Database.DSN = asString(cl.withDefault("database.dsn", "analytics.db"))
-	conf.Datasette.Address = asString(cl.withDefault("datasette.address", "127.0.0.1:8001"))
 
 	return conf, nil
 }
