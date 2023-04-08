c290b82deba478cc741b21fce50c1a19c920043b
AKP <tom@tdpain.net>
2023-04-08 11:37:40 +0100

    Raise maximum query duration
    Signed-off-by: AKP <tom@tdpain.net>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-datasette analytics.db --plugins-dir=../ds/plugins --metadata=../ds/metadata.json --setting base_url $BASE_URL --host 0.0.0.0 &
+datasette analytics.db --plugins-dir=../ds/plugins --metadata=../ds/metadata.json --setting base_url $BASE_URL --host 0.0.0.0 --setting sql_time_limit_ms 5000 &
 DS_PID=$!
 ../ingest
 kill $DS_PID
